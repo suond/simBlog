12240ef4483db2a57dc4a832d95a5554560e6aea
suond <dsuon04@gmail.com>
2019-01-14 03:06:48 -0800

    Added comments to show pages with create and delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+
   def index
     @posts = Post.all.order("created_at DESC")
   end
@@ -19,17 +21,14 @@ class PostsController < ApplicationController
   end
 
   def show
-    #finds the post
-    @post = Post.find(params[:id])
+
   end
 
   def edit
-    @post = Post.find(params[:id])
+
   end
 
   def update
-    @post = Post.find(params[:id])
-
     if @post.update(post_params)
       redirect_to @post
     else
@@ -38,9 +37,7 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-
     redirect_to posts_path
   end
 
@@ -49,4 +46,9 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title,:content)
   end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
 end
